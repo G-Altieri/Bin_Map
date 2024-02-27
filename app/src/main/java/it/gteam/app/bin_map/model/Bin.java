0e68f222ded6467cbe6295a7d53f95192f0c3fa7
GiammarcoUbaldi <ubaldigiammarco19@gmail.com>
2024-02-27 15:11:21 +0100

    Classe Bin

---
+++ app/src/main/java/it/gteam/app/bin_map/model/Bin.java
@@ -0,0 +1,28 @@
+package it.gteam.app.bin_map.model;
+
+
+public class Bin {
+
+    private int id;
+
+    private String comune;
+
+    private String provincia;
+
+    private String regione;
+
+    private double latitudine;
+
+    private double longitudine;
+
+    private String type;
+
+
+
+
+
+
+
+
+
+}

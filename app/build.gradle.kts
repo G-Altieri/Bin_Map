501fce1e619b532501b4d2e662d15c1d759a885a
Giovanni Altieri <79712043+G-Altieri@users.noreply.github.com>
2024-02-27 15:21:12 +0100

    aggiornamento dipendenze per errore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,11 +35,27 @@ android {
 
 dependencies {
 
+    //Soluzione Trovata su internet per risolvere il problema della classe dupplicata
+    constraints {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.0") {
+            because("kotlin-stdlib-jdk7 is now a part of kotlin-stdlib")
+        }
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.0") {
+            because("kotlin-stdlib-jdk8 is now a part of kotlin-stdlib")
+        }
+    }
+
+    //Dipendenze
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.6.0")
+    implementation("com.google.android.material:material:1.6.0") //downgrade per risolvere un errore
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    //navigation ui
     implementation("androidx.navigation:navigation-fragment:2.7.7")
     implementation("androidx.navigation:navigation-ui:2.7.7")
+
+
+    //Altro
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

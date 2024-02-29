13ce3d797121bf923d33c680c53a6d75c77956eb
Giovanni Altieri <79712043+G-Altieri@users.noreply.github.com>
2024-02-29 22:32:36 +0100

    aggiunta img della schermata home

--- app/src/main/java/it/gteam/app/bin_map/MainActivity.java
+++ app/src/main/java/it/gteam/app/bin_map/MainActivity.java
@@ -1,10 +1,13 @@
 package it.gteam.app.bin_map;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
@@ -25,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
 //https://vivicalascio.altervista.org/BinMap/dati.json
     private boolean isDarkModeEnable;
     private ImageView buttonCambioTema;
+    private ImageView buttonArrowBackToolBar;
+
+    private NavController navController;
 
     private SharedPreferences sharedPreferences;
 
@@ -73,33 +79,47 @@ public class MainActivity extends AppCompatActivity {
 
         //Fine Cambio Tema
 
+
+
+        // questo fa funzionare il bottomNavView tramite il click
+        NavHostFragment fragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerViewMainActivity);
+        if (fragment != null) {
+            navController = fragment.getNavController();
+            NavigationUI.setupWithNavController(binding.bottomNavigationView, navController);
+        }
+
         //Codice arrow back
 
         //Button back Arrow
-        ImageView imageView = (ImageView) findViewById(R.id.buttonBack);
-     /*   imageView.setOnClickListener(new View.OnClickListener() {
+         buttonArrowBackToolBar = (ImageView) findViewById(R.id.buttonBack);
+        buttonArrowBackToolBar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                        .findFragmentById(R.id.nav_graph);
-                NavController navCo = navHostFragment.getNavController();
-                navCo.navigate(R.id.menu_home, bundle);
+                navController.navigate(R.id.menu_home);
             }
-        });*/
+        });
 
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                Log.v("Posizione Attuale", "onDestinationChanged: "+destination.getLabel());
+                String paginaCorrente = (String) destination.getLabel();
+                if (paginaCorrente.contains("calendar")) {
+                    Log.v("Posizione AttualeAAAAAAAAAAAA", "SIIIIIIIIIIIII");
+                    buttonArrowBackToolBar.setVisibility(View.VISIBLE);
+                }else{
+                    Log.v("Posizione AttualeAAAAAAAAAAAA", "NOOOOOOOOOOOO");
+                    buttonArrowBackToolBar.setVisibility(View.GONE);
+                }
+
+            }
+        });
 
 
 
         //Fine arrow back
 
 
-
-        // questo fa funzionare il bottomNavView tramite il click
-        NavHostFragment fragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerViewMainActivity);
-        if (fragment != null) {
-            NavController controller = fragment.getNavController();
-            NavigationUI.setupWithNavController(binding.bottomNavigationView, controller);
-        }
     }
 
     private void toggleDarkMode(){
